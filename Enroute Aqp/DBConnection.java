aacf9f1b2b019567aefde20a899e0ab57274b54f
JordiCA2 <u21232573@utp.edu.pe>
2025-09-08 18:52:54 -0500

    se corrige el landing page y se crea el dbconnection y se agrega la libreria.

---
+++ Enroute Aqp/DBConnection.java
@@ -0,0 +1,31 @@
+package com.enrouteaqp.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/enroute_aqp_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL JDBC Driver no encontrado", e);
+        }
+    }
+
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            System.err.println("Error al cerrar la conexion: " + e.getMessage());
+        }
+    }
+}
